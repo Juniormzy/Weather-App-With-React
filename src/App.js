9e880f69421635798534b1d29d250ccf213b15bf
Juniormzy <72698985+Juniormzy@users.noreply.github.com>
2021-10-27 17:06:52 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const dData = {
 };
 function App() {
   const dateGen = (d) =>{
-    let months =[ "January","February","March","April","May","June","July","August","September","Octomber","November","December"];
+    let months =[ "January","February","March","April","May","June","July","August","September","October","November","December"];
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let day = days[d.getDay()];
     let date = d.getDate();
